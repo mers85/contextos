a00b821a8e810bc14410b5f0c56ead2988f647a3
Mayra Rodriguez <mers85@gmail.com>
2018-09-09 19:30:47 +0200

    added some style in index view-

--- app/helpers/contexts_helper.rb
+++ app/helpers/contexts_helper.rb
@@ -23,4 +23,11 @@ module ContextsHelper
     link_to(icon.html_safe, url, options)
   end
 
+  def load_image_for(context)
+    if context.pictures.any?
+      image_tag context.pictures.sample.image, class: "img-fluid"
+    else
+      image_tag "img-news.jpg", class: "img-fluid"
+    end
+  end
 end
